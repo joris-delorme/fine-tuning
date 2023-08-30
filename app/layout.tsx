728558d2aa29fa9dc25666de7adfc4cc9e59adfa
joris-delorme <joris.delorme@epitech.digital>
2023-08-30 18:26:32 +0200

    analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ModeToggle } from '@/components/theme/mode-toggle'
 import { Toaster } from '@/components/ui/toaster'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Toaster  />
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
