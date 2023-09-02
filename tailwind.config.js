327986797357bd2b75e11e5d1469408ac3706e0f
joris-delorme <joris.delorme@epitech.digital>
2023-09-02 12:07:37 +0200

    opengraph-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,7 +73,7 @@ module.exports = {
       transitionTimingFunction: {
         'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
         'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
-        'run': 'cubic-bezier(.36,-0.56,.34,1)',
+        'run': 'cubic-bezier(.36,-0.56,.34,1)'
       }
     },
   },
